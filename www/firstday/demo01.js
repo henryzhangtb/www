d8f25208110f3c51786636ea91b4f8c55ddb5dc6
bin6453 <15219859305@163.com>
2018-05-29 11:31:00 +0800

    workk
    study

---
+++ www/firstday/demo01.js
@@ -0,0 +1,15 @@
+document.write("hellowold");
+/*
+//函数的定义
+function myfunction(){
+alert('hello');
+}
+//函数的调用
+myfunction();
+*/
+function display(first,second){
+alert(first+second);
+}
+var i=1;
+var j=3;
+display(i,j);
